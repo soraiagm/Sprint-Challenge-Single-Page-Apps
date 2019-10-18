967dd719c4159b467cfadfa4f0b775ac796d46ae
Soraia <soraia@Soraias-iMac.local>
2019-10-18 11:53:49 -0400

    mapped over data and imported reactstrap for styling

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function CharacterCard() {
-  return <span>todo: character</span>;
+  return (
+    <div>
+      
+    </div>
+  )
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,23 +1,39 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
+import CharacterCard from "./CharacterCard.js";
 
 export default function CharacterList() {
     const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
+    const getCharacters = () => {
     Axios
         .get("https://rickandmortyapi.com/api/character/")
         .then(response => {
           console.log(response.data.results);
+          
+          setCharacters(response.data.results);
         })
         .catch(error => {
           console.log("Characters were not retried", error);
-        })
+        });
+      }
+      getCharacters();
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>TODO: </h2>
+      <div className="character-list">
+        {characters.map(character => (
+          <CharacterCard key={character.id} character={character}
+                image={character.image}
+                name={character.name}
+                gender={character.gender}
+                species={character.species}/>
+        ))}
+      
+      </div>
     </section>
   );
 }
